02cb18ae71dc7c7a42fa8bc740c2c1ebe494f176
miunzueta3f@gmail.com <mikel>
2015-03-13 13:32:17 +0100

    coment de prueba

--- app/src/main/java/rat/rata2/MainActivity.java
+++ app/src/main/java/rat/rata2/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    //coment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
