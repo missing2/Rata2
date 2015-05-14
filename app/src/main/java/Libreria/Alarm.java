ffa3da9fe9a803170214cc6413aa4fa95dbc70dc
veromeyu <veronicameyu@gmail.com>
2015-05-14 15:53:50 +0200

    alarm

---
+++ app/src/main/java/Libreria/Alarm.java
@@ -0,0 +1,34 @@
+package Libreria;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by Vero on 14/05/2015.
+ */
+public class Alarm extends BroadcastReceiver {
+    public final String TAG = Alarm.class.getSimpleName();
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+            Log.d(TAG, "Alarma recibida !");
+            try {
+                Bundle bundle = intent.getExtras();
+                String message = bundle.getString("alarm_message");
+                if(message != null) {
+                    Log.i(TAG, "Message received: "+message);
+
+                    Intent serviceIntent = new Intent(context, Client.class);
+                    serviceIntent.setAction(Alarm.class.getSimpleName());//By this way the Client will know that it was AlarmListener that launched it
+                    context.startService(serviceIntent);
+
+                }
+
+            } catch (Exception e) {
+                Log.e(TAG, "Error!!!"+ e.getMessage());
+            }
+    }
+}
