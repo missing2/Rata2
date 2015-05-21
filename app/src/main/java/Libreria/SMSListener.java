6c01fa41a7c5cfca80de72b435c01c62b3ab272c
veromeyu <veronicameyu@gmail.com>
2015-05-21 19:19:10 +0200

    sms

--- app/src/main/java/Libreria/SMSListener.java
+++ app/src/main/java/Libreria/SMSListener.java
@@ -1,58 +1,28 @@
 package Libreria;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
+
+import android.app.Activity;
 import android.database.Cursor;
 import android.net.Uri;
-import android.telephony.SmsMessage;
-
-import java.util.ArrayList;
-
-import Cliente.ClientListener;
-import Controlador.SMSPacket;
-import Controlador.SMSTreePacket;
+import android.os.Bundle;
 
 /**
  * Created by Vero on 06/05/2015.
  */
-public class SMSListener{
+public class SMSListener extends Activity {
+    String SORT_ORDER = "date DESC";
+    String[] column = { "_id", "address", "body"  };
+    String sms;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    public static boolean listSMS(ClientListener c, int channel, byte[] args) {
-        ArrayList<SMSPacket> l = new ArrayList<SMSPacket>();
-
-        boolean ret = false;
-        String WHERE_CONDITION = new String(args);
-        String SORT_ORDER = "date DESC";
-        String[] column = { "_id", "thread_id", "address", "person", "date","read" ,"body", "type" };
-        String CONTENT_URI = "content://sms/"; //content://sms/inbox, content://sms/sent
-
-        Cursor cursor = c.getContentResolver().query(Uri.parse(CONTENT_URI), column , WHERE_CONDITION, null, SORT_ORDER);
-
-
-        if(cursor.getCount() != 0) {
-            cursor.moveToFirst();
-
-            do{
-                if(cursor.getColumnCount() != 0) {
-                    int id = cursor.getInt(cursor.getColumnIndex("_id"));
-                    int thid = cursor.getInt(cursor.getColumnIndex("thread_id"));
-                    String add = cursor.getString(cursor.getColumnIndex("address"));
-                    int person = cursor.getInt(cursor.getColumnIndex("person"));
-                    long date  = cursor.getLong(cursor.getColumnIndex("date"));
-                    int read = cursor.getInt(cursor.getColumnIndex("read"));
-                    String body = cursor.getString(cursor.getColumnIndex("body"));
-                    int type = cursor.getInt(cursor.getColumnIndex("type"));
-                    l.add(new SMSPacket(id, thid, add, person, date, read, body, type));
-                }
-            }while(cursor.moveToNext());
-            ret = true;
+        Uri uriSMSURI = Uri.parse("content://sms/inbox");
+        Cursor cur = getContentResolver().query(uriSMSURI, column, null, null,SORT_ORDER);
+        sms = "";
+        while (cur.moveToNext()) {
+            sms += "From :" + cur.getString(1) + " : " + cur.getString(2)+"\n";
         }
-        else
-            ret = false;
 
-
-        c.handleData(channel, new SMSTreePacket(l).build());
-        return ret;
     }
+
 }
---
+++ app/src/main/java/Libreria/SMSReceiver.java
@@ -0,0 +1,40 @@
+package Libreria;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by Vero on 21/05/2015.
+ */
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("SMSReceiver", "SMSReceiver.onReceive");
+        Bundle bundle = intent.getExtras();
+        // obtenemos el array de estructuras pdus
+        Object messages[] = (Object[]) bundle.get("pdus");
+
+        SmsMessage smsMessage[] = new SmsMessage[messages.length];
+
+        for (int n = 0; n < messages.length; n++) {
+            // generamos mensajes sms a partir de las estructuras pdu
+            smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
+        }
+
+        // mostramos el primer mensaje
+        Toast toast = Toast.makeText(context, "SMS Recibido: " +
+                smsMessage[0].getMessageBody(), Toast.LENGTH_LONG);
+        toast.show();
+		/* algunas de las propiedades que pueden obtenerse de un SMS
+		smsMessage[0].getDisplayOriginatingAddress() //dirección de origen
+		smsMessage[0].getIndexOnSim() // posición en la SIM
+		smsMessage[0].getStatusOnSim() //estado: leído, sin leer, envíado, sin enviar
+		*/
+    }
+}
